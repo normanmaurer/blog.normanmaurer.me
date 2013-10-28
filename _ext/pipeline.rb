0d22f8e37dad09e7248674b52e60e4d3801c0112
Norman Maurer <nmaurer@redhat.com>
2013-10-28 14:52:02 +0100

    Cleanup

--- _ext/pipeline.rb
+++
@@ -1,8 +0,0 @@
-require 'bootstrap-sass'
-
-Awestruct::Extensions::Pipeline.new do
-  extension Awestruct::Extensions::Posts.new('/blog')
-  extension Awestruct::Extensions::Indexifier.new
-  # Indexifier *must* come before Atomizer
-  extension Awestruct::Extensions::Atomizer.new :posts, '/feed.atom'
-end
