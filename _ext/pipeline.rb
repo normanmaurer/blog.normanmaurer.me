b905eb1deee1d24f6f2420d0eb462bfb8b61ed7f
Norman Maurer <nmaurer@redhat.com>
2013-10-29 17:42:28 +0100

    Add feed

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -4,7 +4,7 @@ Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Posts.new('/blog')
   extension Awestruct::Extensions::Indexifier.new
   # Indexifier *must* come before Atomizer
-  extension Awestruct::Extensions::Atomizer.new :posts, '/feed.atom'
+  extension Awestruct::Extensions::Atomizer.new :posts, '/blog.atom'
   extension Awestruct::Extensions::Disqus.new()
 
   helper Awestruct::Extensions::GoogleAnalytics
